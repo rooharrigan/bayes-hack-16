aa31693e7fe0d88aaed76af2141d368c2bd9ef59
Jessica <jessica.dene.earley@gmail.com>
2016-04-23 14:13:35 -0700

    added funcationality to take in a list of locations and returns the tuple with the times

--- google-process.py
+++ google-process.py
@@ -1,32 +1,12 @@
 """Process data to and from Google maps"""
 
+import os
 import requests
 
-GOOGLE_MAP_API_KEY="AIzaSyApA_At8_2MYDiIwWrlVkvJXY218hXm5wA"
+GOOGLE_MAP_API_KEY = os.environ.get("GOOGLE_MAP_API_KEY")
 
 
-user_lat = 37.785152
-user_lon = -122.406581
-loc_lat = 37.762028
-loc_lon = -122.470790
-
-def gets_min_travel_to_location_from_user_transit(user_lat, user_lon, loc_lat, loc_lon):
-    """Gets the time that it'll take a user to get to a location via transit"""
-
-    url = "https://maps.googleapis.com/maps/api/directions/json?origin={0},{1}&destination={2},{3}&departure_time=now&traffic_model=best_guess&mode=transit&key={4}".format(str(user_lat), str(user_lon), str(loc_lat), str(loc_lon), str(GOOGLE_MAP_API_KEY))
-
-    r = requests.get(url)
-
-    adict = r.json()
-
-    if "error_message" not in adict:
-
-        arrival_time_raw = adict['routes'][0]['legs'][0]['duration']['text']
-        arrival_time_raw_split = arrival_time_raw.split(":")
-
-    return str(arrival_time_raw_split[0])
-
-def gets_min_travel_to_location_from_user_walk(user_lat, user_lon, loc_lat, loc_lon, transit_mode):
+def gets_min_travel_to_location_from_user(user_lat, user_lon, loc_lat, loc_lon, transit_mode):
     """Gets the time that it'll take a user to get to a location via transit"""
 
     url = "https://maps.googleapis.com/maps/api/directions/json?origin={0},{1}&destination={2},{3}&departure_time=now&traffic_model=best_guess&mode={4}&key={5}".format(str(user_lat), str(user_lon), str(loc_lat), str(loc_lon), str(transit_mode), str(GOOGLE_MAP_API_KEY))
@@ -42,3 +22,15 @@ def gets_min_travel_to_location_from_user_walk(user_lat, user_lon, loc_lat, loc_
 
     return str(arrival_time_raw_split[0])
 
+
+def gets_min_travel_to_location_from_user(user_lat, user_lon, locations):
+    """Gets the time that it'll take a user to get to a location via transit and car"""
+
+    new_locations = []
+
+    for location in locations:
+        transit = gets_min_travel_to_location_from_user(user_lat, user_lon, loc_lat, loc_lon, "transit")
+        car = gets_min_travel_to_location_from_user(user_lat, user_lon, loc_lat, loc_lon, "car")
+        new_locations.append((location[0], location[1]), location[2], location[3], transit, car)
+
+    return new_locations
