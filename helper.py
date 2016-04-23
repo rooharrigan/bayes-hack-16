ea55514cba82e0b31675ab6ee005604417975a18
Ally Mcknight <allyson.tong10@gmail.com>
2016-04-23 13:49:52 -0700

    helper functions added

--- helper.py
+++ helper.py
@@ -3,14 +3,22 @@ import os
 
 
 RIDB_API_KEY = os.environ.get("RIDB_API_KEY")
+
 def get_rec_areas(latitude, longitude):
     """Makes an API call to the RIDB."""
-
 
     payload = {"apikey": RIDB_API_KEY, "limit":"10", "radius":"50", "latitude": latitude, "longitude": longitude}
 
     response = requests.get("https://ridb.recreation.gov/api/v1/recareas", params=payload).json()
 
-    return response
+    rec_areas = []
+
+    for locations in response["RECDATA"]:
+        name = locations["RecAreaName"]
+        lat = locations["RecAreaLatitude"]
+        lon = locations["RecAreaLongitude"]
+        rec_areas.append((name, lat, lon))
+
+    return rec_areas
 
 print get_rec_areas("37.804364", "-122.271114")
